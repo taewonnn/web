8d1b07050a50e0c8d908ec3d55f676cc31fe6b6d
TaeWon Park <thisis_tw94@naver.com>
2023-12-26 21:25:38 +0900

    game js

--- game/main.js
+++ game/main.js
@@ -1,15 +1,50 @@
 'use strict';
 
 const carrotSize = 80;
+const carrotCount = 5;
+const bugCount = 5;
+
 const field = document.querySelector('.game-field');
 const fieldRect = field.getBoundingClientRect();
+const gameBtn = document.querySelector('.game-button');
+const gameTimer = document.querySelector('.game-timer');
+const gameScore = document.querySelector('.game-score');
+
+// 초기값
+let started = false;
+let score = 0;
+let timer = undefined;
+
+gameBtn.addEventListener('click', () => {
+  console.log('click');
+  if (started) {
+    stopGame();
+  } else {
+    startGame();
+  }
+  started = !started;
+});
+
+function startGame() {
+  initGame();
+  showStopButton();
+  showTimerAndScore();
+}
+
+function stopGame() {}
+
+function showStopButton() {
+  const icon = gameBtn.querySelector('.fa-play');
+  icon.classList.add('fa-stop');
+  icon.classList.remove('fa-play');
+}
 
 function initGame() {
   // 벌레와 당근을 생성한 뒤 field에 추가
   console.log(fieldRect);
 
-  addItem('carrot', 5, 'img/carrot.png');
-  addItem('bug', 5, 'img/bug.png');
+  addItem('carrot', carrotCount, 'img/carrot.png');
+  addItem('bug', bugCount, 'img/bug.png');
 }
 
 function addItem(className, count, imgPath) {
@@ -19,7 +54,7 @@ function addItem(className, count, imgPath) {
   const x2 = fieldRect.width - carrotSize;
   const y2 = fieldRect.height - carrotSize;
 
-  for (let i = 0; i < couㅌㅌㅌㅌㅌㅌㅌnt; i++) {
+  for (let i = 0; i < count; i++) {
     const item = document.createElement('img');
     item.setAttribute('class', className);
     item.setAttribute('src', imgPath);
@@ -35,5 +70,3 @@ function addItem(className, count, imgPath) {
 function randomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
-
-initGame();
